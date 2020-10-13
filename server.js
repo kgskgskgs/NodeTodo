fdff0647d573680d705d0da338f0f5e28e2ae7ce
K Ganapathy Subramaniam <kgs@Ks-MacBook-Pro.local>
2020-10-13 23:43:56 +0530

    post -localhost/todos is throwing parser error

---
+++ server.js
@@ -0,0 +1,61 @@
+const http = require("http");
+
+
+const todos = [
+{id: 1, text: 'Todo One'},
+{id: 1, text: 'Todo Two'},
+{id: 1, text: 'Todo Three'}
+]
+ 
+const server = http.createServer((req, res) => {
+  //    console.log(req);
+      const {headers, url, method} = req;
+  //    console.log(headers, url, method);
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("X-Powered-By", "NodeJS");
+  res.setHeader("X-My-HeaderKey", "MyValue");
+
+//   res.write("<h1>Hello World</h1>");
+//   res.write("<h2> Hello Again </h2>");
+
+ let body = [];
+ 
+ let status = 404;
+ const response = {
+     sucess : false,
+     data : null
+ };
+
+if(method === 'GET' && url === '/todos') {
+    status = 200;
+    response.sucess = true;
+    response.data = todos;
+} else if(method === 'POST' && url === '/todos') {
+//    const { id, text} = JSON.parse(body);
+   
+   const { id, text } = JSON.parse(body);
+    todos.push({ id, text});
+    status = 201;
+    response.sucess = true;
+    response.data = todos;
+}
+ req.on('data', chunk => {
+     body.push(chunk);
+ })
+ .on('end', () => {
+     body = Buffer.concat(body).toString();
+     console.log(body)
+ });
+res.writeHead(status, {
+    'Content-Type': 'application/json',
+    'X-Powered-By': 'Node.js'
+});
+
+  res.end(
+      JSON.stringify(response)
+      );
+  });
+  
+const PORT = 5000;
+  
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
